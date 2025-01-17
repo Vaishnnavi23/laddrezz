11bdff7b7ae5371ab73e44c708b53b6e1931b2e5
Vaishnnavi23 <108232381+Vaishnnavi23@users.noreply.github.com>
2025-01-17 18:58:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -145,8 +145,17 @@ function displayContents(subject, grade, lesson) {
         if (resourceLink) {
           const resourceButton = document.createElement('button');
           resourceButton.textContent = buttonText; // Set the button text
+          console.log(${resourceLink}?subject=${encodeURIComponent(subject)}&filePath=${encodeURIComponent(resources)}&grade=${encodeURIComponent(grade)}&lessonName=${encodeURIComponent(lesson)};
+
+
+
+
+
+
+
+
           const urlWithParam = '${resourceLink}?subject=${encodeURIComponent(subject)}';
-          urlWithParam = urlWithParam+'grade=${encodeURIComponent(subject)};
+          //urlWithParam = urlWithParam+'grade=${encodeURIComponent(subject)};
           resourceButton.onclick = () => window.open(urlWithParam, '_blank'); // Open the resource link in a new tab
           contentsContainer.appendChild(resourceButton);
           resourcesAvailable = true;
